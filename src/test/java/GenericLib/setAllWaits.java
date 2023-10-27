19abc3b2e8270a4c42d358c990900470f1b4fde6
shubhammalviya1795 <malviyashubham1795@gmail.com>
2023-10-27 21:15:36 +0530

    AddWaitClass

---
+++ src/test/java/GenericLib/setAllWaits.java
@@ -0,0 +1,40 @@
+package GenericLib;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class setAllWaits {
+
+	private WebDriver driver;
+
+	public setAllWaits() {
+		driver = new ChromeDriver();
+	}
+
+	public void implicitlyWait(WebDriver driver) {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+	public void waitForXpath(String xpath) {
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+	}
+
+	public void waitForPresenceOfElementLocated(String xpath) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+	}
+
+	/*
+	 * Used mostly for verifying correct page has loaded This method waits for an
+	 * xpath to be clicked Will time out after 20 seconds if element not found
+	 */
+
+}
